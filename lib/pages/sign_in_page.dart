79bda188a9a7e06569311042b69823dae36c4e11
Prathmesh Bhardwaj <pbrathmesh@gmail.com>
2024-09-15 00:27:41 +0530

    Describe changes to sign_in_page.dart

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -29,17 +29,13 @@ class _SignInPageState extends State<SignInPage> {
     try {
       final user = await _authService.signInWithEmailAndPassword(email, password);
       if (user != null) {
-        // Set the user's preferred language
         Provider.of<LanguageProvider>(context, listen: false).setLocale(Locale(_selectedLanguage));
-        
-        // Navigate to HomePage if sign-in is successful
         Navigator.pushReplacementNamed(context, '/home');
       }
     } catch (e) {
-      // Handle sign-in errors
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('Sign-in error: $e'),  
+          content: Text('Sign-in error: $e'),
         ),
       );
     } finally {
@@ -52,87 +48,113 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(S.of(context).signIn),
         elevation: 0,
-        backgroundColor: const Color.fromARGB(255, 177, 68, 255),
+        backgroundColor: const Color.fromARGB(255, 154, 27, 227), // Deep Purple
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              S.of(context).welcomeBack,
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: S.of(context).email,
-                prefixIcon: const Icon(Icons.email),
-                border: const OutlineInputBorder(),
-                focusedBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  S.of(context).welcomeBack,
+                  style: const TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 163, 45, 227), // Purple
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: S.of(context).password,
-                prefixIcon: const Icon(Icons.lock),
-                border: const OutlineInputBorder(),
-                focusedBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xD2FF4444), width: 2.0),
-                ),
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            DropdownButton<String>(
-              value: _selectedLanguage,
-              items: [
-                DropdownMenuItem(value: 'en', child: Text(S.of(context).english)),
-                DropdownMenuItem(value: 'es', child: Text(S.of(context).spanish)),
-              ],
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedLanguage = newValue!;
-                });
-              },
-            ),
-            const SizedBox(height: 20),
-            _loading
-                ? const CircularProgressIndicator()
-                : ElevatedButton(
-                    onPressed: _signIn,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 177, 68, 255),
-                      padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                    ),
-                    child: Text(
-                      S.of(context).signIn,
-                      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                const SizedBox(height: 30),
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: S.of(context).email,
+                    labelStyle: const TextStyle(color: Color.fromARGB(255, 141, 45, 232)),
+                    prefixIcon: const Icon(Icons.email, color: Color.fromARGB(255, 138, 36, 233)),
+                    filled: true,
+                    fillColor: Colors.purple[50],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
                     ),
                   ),
-            const SizedBox(height: 16),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/register');
-              },
-              style: TextButton.styleFrom(
-                foregroundColor: const Color.fromARGB(255, 177, 68, 255),
-              ),
-              child: Text(S.of(context).createAccount),
+                  keyboardType: TextInputType.emailAddress,
+                  style: const TextStyle(color: Color.fromARGB(255, 150, 37, 231)),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: S.of(context).password,
+                    labelStyle: const TextStyle(color: Color.fromARGB(255, 152, 42, 241)),
+                    prefixIcon: const Icon(Icons.lock, color: Color.fromARGB(255, 159, 34, 236)),
+                    filled: true,
+                    fillColor: Colors.purple[50],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  obscureText: true,
+                  style: const TextStyle(color: Color.fromARGB(255, 145, 21, 217)),
+                ),
+                const SizedBox(height: 30),
+                DropdownButton<String>(
+                  value: _selectedLanguage,
+                  dropdownColor: Colors.purple[50],
+                  items: [
+                    DropdownMenuItem(value: 'en', child: Text(S.of(context).english)),
+                    DropdownMenuItem(value: 'es', child: Text(S.of(context).spanish)),
+                  ],
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedLanguage = newValue!;
+                    });
+                  },
+                  style: const TextStyle(color: Color.fromARGB(255, 174, 44, 244)),
+                  icon: const Icon(Icons.language, color: Color.fromARGB(255, 151, 32, 236)),
+                ),
+                const SizedBox(height: 30),
+                _loading
+                    ? const Center(child: CircularProgressIndicator())
+                    : ElevatedButton(
+                        onPressed: _signIn,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 158, 41, 242), // Purple
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: Text(
+                          S.of(context).signIn,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                const SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/register');
+                  },
+                  style: TextButton.styleFrom(
+                    foregroundColor: const Color.fromARGB(255, 171, 0, 238),
+                  ),
+                  child: Text(S.of(context).createAccount),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
